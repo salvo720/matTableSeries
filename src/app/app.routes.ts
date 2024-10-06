6c99e9e4d7d60e1b8a0eec125d55c9ee049feccc
salvo720 <salvocelano91@gmail.com>
2024-10-06 18:43:13 +0200

    sync project with stackbliz project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { TableComponent } from "./components/table/table.component";
+import { UserComponent } from "./components/user/user.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        component: HomeComponent,
+    },
+    {
+        path: "home",
+        redirectTo: "",
+    },
+    {
+        path: "standard-mat-table",
+        component: TableComponent,
+    },
+    {
+        path: "standard-mat-table-with-filter",
+        component: TableComponent,
+    },
+    {
+        path: "custom-table",
+        component: TableComponent,
+    },
+    {
+        path: "user",
+        component: UserComponent,
+    },
+    {
+        path: "**",
+        redirectTo: "",
+    },
+];
